53914488107eb52a7a778cf51ce8b43f442f7948
Muxa <balashovmb@gmail.com>
2020-07-07 15:12:37 +0500

    Show issues list

--- src/get-issues.js
+++ src/get-issues.js
@@ -28,12 +28,26 @@ function getIssues() {
         try {
             const issues = await loadIssues();
             console.log(issues);
+            renderIssues(issues);
         } catch (e) {
             console.error(e);
         }
     };
 
+    const renderIssues = function (issues) {
+        const issuesRoot = document.getElementById('issues-root');
+        issuesRoot.innerHTML = '';
+        issues.forEach(issue => appendIssue(issuesRoot, issue));
+    }
+
+    function appendIssue(container, issue) {
+        const {number, created_at, title, body} = issue;
+        const dateIssue = created_at.substr(0,10);
+        const div = document.createElement('div');
+        div.innerHTML = `${number}  ${dateIssue} ${title} <br> ${body} <hr>`;
+        container.append(div);
+    }
+
     searchButton.addEventListener('click', showResult)
 }
-
 getIssues();
