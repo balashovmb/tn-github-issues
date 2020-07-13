5765930167c91186de289d1c350590bf58a7d483
balashovmb <balashovmb@gmail.com>
2020-07-13 16:18:50 +0500

    Show user in issue

--- src/types/issue.ts
+++ src/types/issue.ts
@@ -1,8 +1,11 @@
+import User from './user';
+
 type Issue = {
     number:number,
     title:string,
     body:string,
     // eslint-disable-next-line camelcase
     created_at:string,
+    user: User
   }
 export default Issue;
--- src/views/issue-html-string.ts
+++ src/views/issue-html-string.ts
@@ -2,18 +2,20 @@ import Issue from '../types/issue';
 
 function issueHTMLString(issue:Issue):string {
   const {
-    number, title, body,
+    number, title, body, user,
   } = issue;
   const issueDate:string = issue.created_at.substr(0, 10);
-  let issueHTML = `${number} ${issueDate} ${title}`;
+  let issueHTML = `<p> ${number}  ${title} </p>`;
   if (body) {
-    issueHTML += '<br>';
+    issueHTML += '<p>';
     if (body.length < 50) {
       issueHTML += body;
     } else {
       issueHTML += body.substr(0, 50);
     }
+    issueHTML += '</p>';
   }
+  issueHTML += `<img src="${user.avatar_url}" width="30"> <a href='${user.url}'>${user.login}</a> ${issueDate}`;
   issueHTML += '<hr>';
   return issueHTML;
 }
